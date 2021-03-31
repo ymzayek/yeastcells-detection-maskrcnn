9099bca731ede1ae5806670b33c9ceb8298bbd6f
Yasmin Mzayek <mzayeky@gmail.com>
2021-03-31 14:18:55 +0200

    get_track_performance fix

--- yeastcells/evaluation.py
+++ yeastcells/evaluation.py
@@ -116,11 +116,11 @@ def get_track_performance(pred_t, gt_t, output, pipeline='maskrcnn'):
     split = len(split[c_1>1])
     
     #calculate by # of correct links
-    n_matched_links = sum(np.array(list(Counter(labels_matched).values()))-1)
+    _matched_links = sum(np.array(list(Counter(labels_matched).values()))-1) - split - join 
     pred_number_of_links = sum(np.array(list(Counter(pred[:,1]).values()))-1)
     gt_number_of_links = sum(np.array(list(Counter(gt[:,1]).values()))-1)
-    fn = gt_number_of_links - n_matched_links
-    fp = pred_number_of_links - n_matched_links
+    fn = gt_number_of_links - n_matched_links + split
+    fp = pred_number_of_links - n_matched_links + join 
     
     return {
         "tp": n_matched_links, "fp": fp, "fn": fn,
